b087eb7059debc40bd9083086356c4ef49c90a05
Rodrigo Araújo <rodrigo.araujo@jusbrasil.com.br>
2015-07-29 08:18:47 -0300

    v2

---
+++ Perceptron_Python/v2/perceptron.py
@@ -0,0 +1,70 @@
+import math
+from numpy import random, array
+from random import choice
+import matplotlib.pyplot as plt
+
+"""
+This is single unit perceptron that learns simple boolean functions
+this version uses sigmoid function as activation
+in the plot you can see that the loss is decreasing over epochs
+"""
+
+class Perceptron():
+    
+    """
+    this is the thereshold to activate the unit 
+    """
+    def activation(self,x):
+        return 1/(1 + math.exp(-x))
+
+    def linear_combination(self, X):
+        total = 0
+        for i in xrange(len(self.weights)):
+            total += self.weights[i] * X[i]
+        return total
+    
+    def unit_output(self, X):
+        return self.activation(self.linear_combination(X))
+
+    def train(self, training_data, epochs):
+        
+        self.learning_rate = 0.5
+        self.errors = []
+
+        # initializing weights
+        self.weights = random.rand(len(training_data[1][0]))
+        
+        for i in xrange(epochs):
+            X, y = choice(training_data)
+            # > calculate output with current weight
+            output = self.unit_output(X)
+            # > calculate error rate (t - o)
+            error_rate = y - output
+            self.errors.append(error_rate)
+            print 'the X: ', X
+            print 'output: ',output
+            print 'correct target: ', y
+            # > apply learning rule which will update weights
+            self.weights += self.learning_rate * error_rate * X
+
+    def predict(self,X):
+        y = self.unit_output(X)
+        return y
+                
+
+training_data = [
+                (array([0,0,1]), 0),
+                (array([0,1,1]), 0),
+                (array([1,0,1]), 0),
+                (array([1,1,1]), 1),
+                ]
+
+p = Perceptron()
+
+p.train(training_data, 2000)
+print p.predict([1,0,1])
+plt.plot(p.errors)
+plt.ylabel('errors')
+plt.xlabel('epochs')
+plt.show()
+
